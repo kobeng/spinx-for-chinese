d1c313143b6b9a45ba7ec8588c92727f9f199e2f
kobeng <kobeng@hp-ubuntu.(none)>
2013-04-05 02:39:21 +0800

    fix bug

--- shell/build_main_index.sh
+++ shell/build_main_index.sh
@@ -6,7 +6,7 @@ cd $spinx_distributed_system_path/9400/
 for file in *
 do
     echo $file;
-    result=`php $spinx_distributed_system_path/shell/truncate_master_temp_table.php $file`;
+    result=`php $spinx_distributed_system_path/../shell/truncate_master_temp_table.php $file`;
     eval $result;
 done
 
@@ -15,7 +15,7 @@ cd $spinx_distributed_system_path/main_index/
 for file in *
 do
     if [ -d $file ]; then
-        sudo /usr/local/sphinx-for-chinese/bin/indexer -c /home/sphinx-for-chinese/etc/distributed/$file.conf --rotate --all
+        sudo /usr/local/sphinx-for-chinese/bin/indexer -c $spinx_distributed_system_path/$file.conf --rotate --all
     fi
 done
 
